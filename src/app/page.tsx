75abe7bdf0f55c69b3ebbf1196287c03e6e149f7
Austin <babelontrip@gmail.com>
2024-10-09 14:20:44 -0600

    produc section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,6 @@ export default function Home() {
     <main className="w-full">
       <HeroSection />
       <PhraseSection />
-
       <ProductsSection />
     </main>
   )
--- src/components/products-section.tsx
+++ src/components/products-section.tsx
@@ -1,4 +1,34 @@
+/* eslint-disable @next/next/no-img-element */
 export function ProductsSection() {
+  const products = [
+    {
+      url: 'https://i.insider.com/63e3d30827e5db0018eebf41?width=800&format=jpeg&auto=webp'
+    },
+    {
+      url: 'https://i.insider.com/63e3d30827e5db0018eebf41?width=800&format=jpeg&auto=webp'
+    },
+    {
+      url: 'https://i.insider.com/63e3d30827e5db0018eebf41?width=800&format=jpeg&auto=webp'
+    },
+    {
+      url: 'https://i.insider.com/63e3d30827e5db0018eebf41?width=800&format=jpeg&auto=webp'
+    },
+    {
+      url: 'https://au.benetton.com/on/demandware.static/-/Sites-ucb-master/default/dw3bac4fe5/images/Full_Card_h/Benetton_23A_2JF8UN02Q_313_FY_Full_Card_h.jpg'
+    },
+    {
+      url: 'https://au.benetton.com/on/demandware.static/-/Sites-ucb-master/default/dw3bac4fe5/images/Full_Card_h/Benetton_23A_2JF8UN02Q_313_FY_Full_Card_h.jpg'
+    },
+    {
+      url: 'https://au.benetton.com/on/demandware.static/-/Sites-ucb-master/default/dw3bac4fe5/images/Full_Card_h/Benetton_23A_2JF8UN02Q_313_FY_Full_Card_h.jpg'
+    },
+    {
+      url: 'https://dtcralphlauren.scene7.com/is/image/PoloGSI/s7-1463368_alternate3?$rl_4x5_pdp$'
+    },
+    {
+      url: 'https://dtcralphlauren.scene7.com/is/image/PoloGSI/s7-1463368_alternate3?$rl_4x5_pdp$'
+    }
+  ]
   return (
     <section>
       <nav className="">
@@ -17,9 +47,31 @@ export function ProductsSection() {
           </ul>
         </div>
       </nav>
-      <div>
-        <div className="card-item">
-          <img src="" alt="" />
+      {/* PRODUCTS */}
+      <div className="mt-5">
+        <div className="product-wrapper flex flex-wrap gap-2">
+          {products.map(({ url }, index) => {
+            return (
+              <div
+                className="card-product flex-col flex hover:cursor-pointer"
+                key={index}
+              >
+                <div className="flex-auto max-w-[300px] overflow-hidden max-h-[300px] aspect-[4/3] rounded-xl">
+                  <img
+                    className="w-full h-ful object-cover"
+                    src={url}
+                    alt="model man"
+                  />
+                </div>
+                <div className="mt-5 flex flex-col">
+                  <span className="font-rubik uppercase m-0">cord shirt</span>
+                  <span className="font-bold text-stone-400 m-0 max-h-min">
+                    $100
+                  </span>
+                </div>
+              </div>
+            )
+          })}
         </div>
       </div>
     </section>
